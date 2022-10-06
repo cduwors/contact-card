2a1dbbb8f1557e7bc6524a788784dbcb7f80b1b8
cduwors <christinaduwors@gmail.com>
2022-10-06 14:00:41 -0400

    created, configured, and connected IndexedDB database

---
+++ client/src/js/index.js
@@ -0,0 +1,19 @@
+import { initdb } from "./database";
+import "./form";
+import "./submit";
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
+
+window.addEventListener("load", function () {
+	initdb();
+	document.getElementById("logo").src = Logo;
+	document.getElementById("bearThumbnail").src = Bear;
+	document.getElementById("dogThumbnail").src = Dog;
+});
+
+// Import CSS files
+import "../css/index.css";
